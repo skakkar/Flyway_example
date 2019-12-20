0334038defac8eb65084873356a662e3d21fac32
Sachin Kakkar <sachinkakar@gmail.com>
2019-12-20 17:06:57 +0530

    Added code for Flyway example with SpringBoot

---
+++ src/main/java/com/example/flyway/FlywayExampleApplication.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.example.flyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author skakkar
+ *
+ */
+@SpringBootApplication
+@RestController
+public class FlywayExampleApplication {
+	
+	public static void main(String args[]) {
+		SpringApplication.run(FlywayExampleApplication.class, args);
+	}
+	
+	@RequestMapping(value = "/")
+	   public String hello() {
+	      return "Hello World";
+	   }
+
+}
